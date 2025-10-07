ab18cc0e7e403beda3b8021b1e06b8e154579722
Gustavo Ramos S. Silva <58111532+gustavo-rss@users.noreply.github.com>
2025-10-07 21:23:29 +0100

    test: add test "It goes back to the customers list when clicking the 'Back' button *"

--- cypress/e2e/EngageSphere.cy.js
+++ cypress/e2e/EngageSphere.cy.js
@@ -2,7 +2,18 @@ describe("EngageSphere", () => {
   beforeEach(() => cy.visit("http://localhost:3000/"));
   beforeEach(() => cy.contains("button", "Accept").click());
 
-  it("It keeps the filters when coming back from the customer details view", () => {
+  it("It goes back to the customers list when clicking the 'Back' button *", () => {
+    // Act
+    cy.get('button[aria-label*="View company:"]').first().click();
+    cy.contains("button", "Back").click();
+
+    // Assert
+    cy.get('select[data-testid="size-filter"]').should("be.visible");
+    cy.get('select[data-testid="industry-filter"]').should("be.visible");
+    cy.get('[data-testid="table"]').should("be.visible");
+  });
+
+  it("It keeps the filters when coming back from the customer details view *", () => {
     // Arrange
     cy.get('select[data-testid="size-filter"]').select("Large Enterprise");
     cy.get('select[data-testid="industry-filter"]').select("HR");
