e8f7024661a9ab63f623abaf8d5ec7a2c98bcebe
Gustavo Ramos S. Silva <58111532+gustavo-rss@users.noreply.github.com>
2025-10-15 22:17:26 +0100

    test: add tests "It shows the cookie banner on first visit" "It accepts the cookie banner on first visit" "It renders the 'Hi there' greeting when no name is provided" "It renders the 'Hi Joe' greeting when name is provided"

--- cypress/e2e/EngageSphere.cy.js
+++ cypress/e2e/EngageSphere.cy.js
@@ -4,7 +4,7 @@ describe("EngageSphere", () => {
     cy.visit("/");
   });
 
-  it("It goes back to the customers list when clicking the 'Back' button *", () => {
+  it("It goes back to the customers list when clicking the 'Back' button", () => {
     // Arrange
     cy.contains("button", "View").click();
 
@@ -17,7 +17,7 @@ describe("EngageSphere", () => {
     cy.get('[data-testid="table"]').should("be.visible");
   });
 
-  it("It keeps the filters when coming back from the customer details view *", () => {
+  it("It keeps the filters when coming back from the customer details view", () => {
     // Arrange
     cy.get('select[data-testid="size-filter"]').select("Large Enterprise");
     cy.get('select[data-testid="industry-filter"]').select("HR");
@@ -31,7 +31,7 @@ describe("EngageSphere", () => {
     cy.get('option[value="HR"]').should("be.visible").and("be.selected");
   });
 
-  it("It renders the footer with the right text and links *", () => {
+  it("It renders the footer with the right text and links", () => {
     // Arrange
     const footerContainer = 'footer[class^="Footer_container"]';
 
@@ -53,5 +53,43 @@ describe("EngageSphere", () => {
     cy.contains(`${footerContainer} a`, "YouTube")
       .should("be.visible")
       .and("have.attr", "href", "https://youtube.com/@talkingabouttesting");
+  });
+
+  it("It shows the cookie banner on first visit", () => {
+    // Arrange
+    cy.clearCookie("cookieConsent");
+    cy.reload();
+
+    // Assert
+    cy.get('[class^="CookieConsent_banner"]').should("be.visible");
+    cy.contains("button", "Accept").should("be.visible");
+    cy.contains("button", "Decline").should("be.visible");
+  });
+
+  it("It accepts the cookie banner on first visit", () => {
+    // Arrange
+    cy.clearCookie("cookieConsent");
+    cy.reload();
+    cy.get('[class^="CookieConsent_banner"]').should("be.visible");
+
+    // Act
+    cy.contains("button", "Accept").click();
+
+    // Assert
+    cy.get('[class^="CookieConsent_banner"]').should("not.exist");
+  });
+
+  it("It renders the 'Hi there' greeting when no name is provided", () => {
+    // Assert
+    cy.get('[data-testid="name"]').should("be.empty");
+    cy.contains("h2", "Hi there").should("be.visible");
+  });
+
+  it("It renders the 'Hi Joe' greeting when name is provided", () => {
+    // Act
+    cy.get('[data-testid="name"]').type("Joe");
+
+    // Assert
+    cy.contains("h2", "Hi Joe").should("be.visible");
   });
 });
