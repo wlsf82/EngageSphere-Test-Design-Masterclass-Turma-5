777b4a7b35162982c18e1a64e70abb2dfa0a511a
Bruno Pulis <brunopulis@protonmail.com>
2025-10-22 21:05:39 -0300

    refactor: Refactoring the testing code

--- cypress/e2e/gui/customer.spec.cy.js
+++ cypress/e2e/gui/customer.spec.cy.js
@@ -1,55 +1,47 @@
 /// <reference types="cypress" />
 
-/*
-  Retorna à lista de clientes ao clicar no botão "Voltar" *
-  Exibe o rodapé com o texto e links corretos *
-  Exibe o cabeçalho com um título, alternador de tema e um campo de entrada de texto *
-  Abre e fecha o messenger *
-  Garante que todos os campos do messenger são obrigatórios e que o primeiro está focado *
-  Mostra e oculta uma mensagem de sucesso ao enviar o formulário do messenger com sucesso *
-  Limpa todos os campos do formulário do messenger ao preenchê-los, fechar o messenger e abri-lo novamente *
-  Mostra as colunas Nome da Empresa e Ação, e oculta as colunas ID, Indústria, Número de Funcionários e Tamanho em um viewport móvel *
-*/
-
 describe('EngageSphere Testing App', () => {
+  const COOKIE_CONSENT = 'div[class^="CookieConsent_banner__"]'
+
   beforeEach(() => {
     cy.visit('/')
   })
 
   it('should display the banner on the first visit', () => {
-    cy.get('body')
-      .get('.CookieConsent_banner__UHArL')
-      .should('be.visible')
+    cy.get(COOKIE_CONSENT).should('be.visible')
 
-    cy.contains('Accept').should('be.visible')
-    cy.contains('Decline').should('be.visible')
+    cy.contains('button', 'Accept').should('be.visible')
+    cy.contains('button', 'Decline').should('be.visible')
+
     cy.getCookies().should('be.empty')
   })
 
-  it('Should be accepted the consent cookies', () => {
-    cy.get('body')
-      .get('.CookieConsent_banner__UHArL')
-      .get('.Button_buttonContainer__X5AJ- button:first-child')
-      .contains('Accept')
+  it('Should accept the cookies consent banner', () => {
+    cy.get(COOKIE_CONSENT)
+
+    cy.contains('button', 'Accept')
+      .should('be.visible')
       .click()
 
     cy.setCookie('cookieConsent', 'accepted')
     cy.getCookie('cookieConsent').should('exist')
+    cy.getCookie('cookieConsent').should('have.property', 'value', 'accepted')
   })
 
   it('Displays the greeting “Hi, there” when no name is provided', () => {
-    cy.grettings('Hi there!')
+    cy.contains('h2', 'Hi there!').should('be.visible')
   })
 
   it('Displays the greeting “Hi, {name}” when the name is provided', () => {
     cy.get('[data-testid=name]')
       .type('Pulis')
-    cy.grettings('Hi Pulis!')
+
+    cy.contains('h2', 'Hi Pulis!').should('be.visible')
   })
 
-  it('Show messenger', () => {
-    cy.get('body')
-      .get('.Messenger_openCloseButton__OgKIA')
+  it('Open chat messenger', () => {
+    cy.get('button[class^="Messenger_openCloseButton__"]')
+      .should('be.visible')
       .click()
   })
 })
